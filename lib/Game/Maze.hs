02b30963b61f4b9b1cc2cbde806df1b764cd175e
Vonfry <mail@vonfry.name>
2021-09-11 17:46:22 +0800

    maze: frame shortest escape path

--- lib/Game/Maze.hs
+++ lib/Game/Maze.hs
@@ -5,11 +5,14 @@ module Game.Maze (MovableCell(..), Maze2D(..)) where
 import Data.Function (on)
 import Data.Foldable (minimumBy)
 import Data.Ord (comparing)
+import Data.Map (Map(..), empty)
+import Control.Monad.State
+import Control.Monad.Reader
 
 class MovableCell cell where
   movable :: cell -> Bool
 
-class Maze2D map pos cell where
+class Eq pos => Maze2D map pos cell where
   adjacence :: map pos cell -> pos -> [pos]
 
   source :: map pos cell -> [pos]
@@ -19,8 +22,18 @@ class Maze2D map pos cell where
   getCell :: map pos cell -> pos -> cell
 
   shortestEscapePath :: map pos cell -> [pos]
-  shortestEscapePath = filterMinLength . fmap bfs . source
+  shortestEscapePath = flip evalState empty . runBfs
     where
-      filterMinLength = minimumBy $ comparing length
-      -- TODO
-      bfs _ = []
+      runBfs = runReaderT =<< bfs . source
+      bfs :: [pos] -> RMapEscape map pos
+      bfs _ = pure []
+
+data MapEscapeMark pos =
+  MapEscapeMark { marked :: Bool
+                , toDest :: [pos]
+                }
+type RMapEscapeState pos = State (Map pos (MapEscapeMark pos))
+type RMapEscape map pos = ReaderT map (RMapEscapeState pos) [pos]
+
+filterMinLength :: [[pos]] -> [pos]
+filterMinLength = minimumBy $ comparing length
